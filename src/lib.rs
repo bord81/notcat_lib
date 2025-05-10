9fe926ed246492883f677e63ff4a6434370df5b8
Borys Zakaliuk <bslabut@gmail.com>
2025-05-10 13:16:11 +0200

    add rust and jni libs

--- src/lib.rs
+++ src/lib.rs
@@ -124,4 +124,74 @@ pub unsafe extern "C" fn notcat_close(handle: *mut NotCatClientHandle) -> c_int 
     }
 }
 
-//TODO: Add JNI bindings for the above functions
+extern crate jni;
+
+#[cfg(target_os = "android")]
+mod notcat_jni {
+    use super::NotCatClient;
+    use jni::objects::{JClass, JString};
+    use jni::sys::{jint, jlong};
+    use super::jni::JNIEnv;
+
+    fn jstring_to_string(env: &JNIEnv, js: JString) -> Option<String> {
+        env.get_string(js).ok().map(|s| s.into())
+    }
+
+    // Methods signature correspond to top level Kotlin functions
+    // Java signature suppport implies class name instead of file name
+    #[no_mangle]
+    pub extern "system" fn Java_com_notcat_NotCatClientKt_nativeConnect(
+        env: JNIEnv,
+        _class: JClass,
+        jpath: JString,
+    ) -> jlong {
+        let path = match jstring_to_string(&env, jpath) {
+            Some(s) => s,
+            None => return 0,
+        };
+        match NotCatClient::connect(path) {
+            Ok(client) => {
+                let boxed = Box::new(client);
+                Box::into_raw(boxed) as jlong
+            }
+            Err(_) => 0,
+        }
+    }
+
+    #[no_mangle]
+    pub extern "system" fn Java_com_notcat_NotCatClientKt_nativeLog(
+        env: JNIEnv,
+        _class: JClass,
+        handle: jlong,
+        jmsg: JString,
+    ) -> jint {
+        if handle == 0 {
+            return -1;
+        }
+        let client: &mut NotCatClient = unsafe { &mut *(handle as *mut NotCatClient) };
+        let msg = match jstring_to_string(&env, jmsg) {
+            Some(s) => s.into_bytes(),
+            None => return -1,
+        };
+        match client.log(&msg) {
+            Ok(()) => 0,
+            Err(_) => -1,
+        }
+    }
+
+    #[no_mangle]
+    pub extern "system" fn Java_com_notcat_NotCatClientKt_nativeClose(
+        _env: JNIEnv,
+        _class: JClass,
+        handle: jlong,
+    ) -> jint {
+        if handle == 0 {
+            return -1;
+        }
+        let boxed: Box<NotCatClient> = unsafe { Box::from_raw(handle as *mut NotCatClient) };
+        match boxed.close() {
+            Ok(()) => 0,
+            Err(_) => -1,
+        }
+    }
+}
